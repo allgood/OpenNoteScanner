90e772e51a768bf80ed1e540fef1fd3d6872f822
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-10-09 12:23:32 -0300

    ClickListener to Kotlin

--- app/src/main/java/com/todobom/opennotescanner/ImageProcessor.java
+++ app/src/main/java/com/todobom/opennotescanner/ImageProcessor.java
@@ -19,7 +19,6 @@ import com.google.zxing.LuminanceSource;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Result;
-import com.google.zxing.ResultPoint;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.multi.qrcode.QRCodeMultiReader;
 import com.todobom.opennotescanner.helpers.OpenNoteMessage;
