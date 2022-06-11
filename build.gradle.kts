f28306826d0408aed7659c63510ca5f32385073f
Patryk Mis <patryk.mis@mykolab.com>
2022-06-11 17:22:07 +0200

    Bump kotlin-gradle-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
-        classpath(kotlin("gradle-plugin", version = "1.6.21"))
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
