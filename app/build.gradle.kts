a8f0294da2559c20e62a48afd96418eaa194df8a
Patryk Miś <patryk.mis@mykolab.com>
2022-10-27 15:15:34 +0200

    build: updates (#317)
    * Update Gradle Wrapper
    
    * build: updates
    
    * move away from deprecated `package` attribute
    * bump dependencies
    * bump CompileSdk and BuildTools
    
    * fix: properly reference exifinterface
    
    * style: reformat
    
    * build: add existing androidx.fragment
    
    * Bump iTextPDF version
    
    * Bump AndroidX
    
    Co-authored-by: PatrickMis <24607131+PatrickMis@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,9 @@ plugins {
 }
 
 android {
-    compileSdk = 32
-    buildToolsVersion = "32.0.0"
+    compileSdk = 33
+    buildToolsVersion = "33.0.0"
+    namespace = "com.todobom.opennotescanner"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -23,7 +24,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     flavorDimensions += "version"
@@ -47,12 +48,13 @@ android {
 dependencies {
     implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
 
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.20")
 
-    implementation("androidx.exifinterface:exifinterface:1.3.3")
+    implementation("androidx.exifinterface:exifinterface:1.3.5")
     testImplementation("junit:junit:4.13.2")
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.4")
     implementation("com.google.android.material:material:1.6.1")
     implementation("com.google.zxing:core:3.5.0")
     implementation("com.github.ctodobom:OpenCV-3.1.0-Android:9e00ee4218ca0c9e60a905c9f09bf499f9dc5115")
@@ -61,9 +63,9 @@ dependencies {
     implementation("com.github.allgood:Android-Universal-Image-Loader:717a00c")
     implementation("com.github.ctodobom:FabToolbar:3c5f0e0ff1b6d5089e20b7da7157a604075ae943")
     implementation("com.github.matomo-org:matomo-sdk-android:4.1.4")
-    implementation("com.github.MikeOrtiz:TouchImageView:3.2.0")
+    implementation("com.github.MikeOrtiz:TouchImageView:3.2.1")
 
-    val itextpdf_version = "7.2.3"
+    val itextpdf_version = "7.2.4"
     implementation("com.itextpdf:kernel:$itextpdf_version")
     implementation("com.itextpdf:layout:$itextpdf_version")
     implementation("com.itextpdf:io:$itextpdf_version")
